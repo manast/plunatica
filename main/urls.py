eabf0e61bfa4041637df9f9deec0823de7f96587
Manuel Astudillo <manuel@jacaras.com>
2009-06-16 22:38:46 +0200

    changed url blog detail resolution to use a named URL pattern. This will help to decouple the blog from the rest of the site.

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('main.views', 
                         (r'^$', 'main'),
                         (r'^(?P<blog_id>\d+)/$', 'blog_detail'),
-                        (r'^(?P<blog_slug>\w+-?\w)+/$', 'blog_detail')
+                        url(r'^(?P<blog_slug>\w+-?\w)+/$', 'blog_detail', name='blog-detail')
 )
 
 # r'^articles/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d+)/$'
