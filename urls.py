3b1c320d3b592bab5c3c2be666c99843a2634a42
Manuel Astudillo <manuel@jacaras.com>
2009-06-23 23:17:38 +0200

    Added feed support.

--- urls.py
+++ urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls.defaults import *
+from plunatica.blog.models import LatestEntries
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+feeds = {'latest': LatestEntries}
+
+
 urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
     (r'^', include ('plunatica.main.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
@@ -12,8 +18,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-    
+
     # Static Media
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': '/Users/manuel/Django-projects/plunatica/media'})
