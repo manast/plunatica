852847163e56ad1c4157c18e47b38b1fc1e73e67
Manuel Astudillo <manuel@jacaras.com>
2009-07-01 21:43:48 +0200

    Added a view for previewing an article on the admin interface.

--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,8 @@
 
-# empty because this is a plugable app.
+from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic.list_detail import object_detail
+from blog.models import Blog
+
+@staff_member_required
+def preview(request, object_id):
+    return object_detail(request, object_id=object_id, queryset=Blog.objects.all(), template_object_name = 'blog', )
